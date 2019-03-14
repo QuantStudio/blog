a6bc64a63f36a6c6866bb839e8259777077cc0db
G_will <g_will@ieqi.com>
2019-03-14 14:59:43 +0800

    update

--- tailwind.js
+++ tailwind.js
@@ -192,43 +192,53 @@ screens: {
 |
 */
 
-// fonts: {
-//   'sans': [
-//     'Nunito Sans',
-//     'system-ui',
-//     'BlinkMacSystemFont',
-//     '-apple-system',
-//     'Segoe UI',
-//     'Roboto',
-//     'Oxygen',
-//     'Ubuntu',
-//     'Cantarell',
-//     'Fira Sans',
-//     'Droid Sans',
-//     'Helvetica Neue',
-//     'sans-serif',
-//   ],
-//   'serif': [
-//     'Constantia',
-//     'Lucida Bright',
-//     'Lucidabright',
-//     'Lucida Serif',
-//     'Lucida',
-//     'DejaVu Serif',
-//     'Bitstream Vera Serif',
-//     'Liberation Serif',
-//     'Georgia',
-//     'serif',
-//   ],
-//   'mono': [
-//     'Menlo',
-//     'Monaco',
-//     'Consolas',
-//     'Liberation Mono',
-//     'Courier New',
-//     'monospace',
-//   ]
-// },
+fonts: {
+  'sans': [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'PingFang SC',
+    'Hiragino Sans GB',
+    'Microsoft YaHei',
+    'Helvetica Neue',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol'
+  ],
+  'serif': [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'PingFang SC',
+    'Hiragino Sans GB',
+    'Microsoft YaHei',
+    'Helvetica Neue',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol'
+  ],
+  'mono': [
+    '-apple-system',
+    'BlinkMacSystemFont',
+    'Segoe UI',
+    'PingFang SC',
+    'Hiragino Sans GB',
+    'Microsoft YaHei',
+    'Helvetica Neue',
+    'Helvetica',
+    'Arial',
+    'sans-serif',
+    'Apple Color Emoji',
+    'Segoe UI Emoji',
+    'Segoe UI Symbol'
+  ]
+},
 
 
 /*
