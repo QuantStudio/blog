1d63c211e3dd329a582a2a46efea7be169d5ed02
G_will <g_will@ieqi.com>
2019-03-14 14:06:47 +0800

    update

--- tailwind.js
+++ tailwind.js
@@ -192,43 +192,43 @@ screens: {
 |
 */
 
-fonts: {
-  'sans': [
-    'Nunito Sans',
-    'system-ui',
-    'BlinkMacSystemFont',
-    '-apple-system',
-    'Segoe UI',
-    'Roboto',
-    'Oxygen',
-    'Ubuntu',
-    'Cantarell',
-    'Fira Sans',
-    'Droid Sans',
-    'Helvetica Neue',
-    'sans-serif',
-  ],
-  'serif': [
-    'Constantia',
-    'Lucida Bright',
-    'Lucidabright',
-    'Lucida Serif',
-    'Lucida',
-    'DejaVu Serif',
-    'Bitstream Vera Serif',
-    'Liberation Serif',
-    'Georgia',
-    'serif',
-  ],
-  'mono': [
-    'Menlo',
-    'Monaco',
-    'Consolas',
-    'Liberation Mono',
-    'Courier New',
-    'monospace',
-  ]
-},
+// fonts: {
+//   'sans': [
+//     'Nunito Sans',
+//     'system-ui',
+//     'BlinkMacSystemFont',
+//     '-apple-system',
+//     'Segoe UI',
+//     'Roboto',
+//     'Oxygen',
+//     'Ubuntu',
+//     'Cantarell',
+//     'Fira Sans',
+//     'Droid Sans',
+//     'Helvetica Neue',
+//     'sans-serif',
+//   ],
+//   'serif': [
+//     'Constantia',
+//     'Lucida Bright',
+//     'Lucidabright',
+//     'Lucida Serif',
+//     'Lucida',
+//     'DejaVu Serif',
+//     'Bitstream Vera Serif',
+//     'Liberation Serif',
+//     'Georgia',
+//     'serif',
+//   ],
+//   'mono': [
+//     'Menlo',
+//     'Monaco',
+//     'Consolas',
+//     'Liberation Mono',
+//     'Courier New',
+//     'monospace',
+//   ]
+// },
 
 
 /*
